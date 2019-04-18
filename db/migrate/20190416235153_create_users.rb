9c89d5dee98c2fd6771652c01ddf183c1ba43dea
Paul Hoffer <git@paulhoffer.com>
2019-04-18 10:09:28 -0700

    wip tweaks

--- db/migrate/20190416235153_create_users.rb
+++ db/migrate/20190416235153_create_users.rb
@@ -1,7 +1,9 @@
 class CreateUsers < ActiveRecord::Migration[5.2]
   def change
+    enable_extension(:citext)
     create_table :users do |t|
       t.citext :email
+      t.index :email, unique: true
 
       t.timestamps
     end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,18 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  setup do
+    @email = "ASDF@example.com"
+    @user = User.create(email: @email)
+  end
+  test "looks up case insensitive email" do
+    assert_equal @user, User.find_by(email: @email.downcase)
+  end
+  test "can't duplicate case insensitive emails" do
+    # DB constraint will raise an error
+    # assert_raise(ActiveRecord::RecordNotUnique) { User.create(email: @email.downcase) }
+
+    # validates_uniqueness_of will return a user object which is not persisted to the DB, and has an error on it
+    assert_equal ["has already been taken"], User.create(email: @email.downcase).errors.messages[:email]
+  end
 end
